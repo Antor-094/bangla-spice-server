75b0eb8f2f880b8390d50ecb81d0ee34eb1e4113
Antor-094 <mdantor556677889@gmail.com>
2023-05-03 17:54:48 +0600

    path for chefs data api added

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const port = process.env.PORT || 5000;
 const app = express()
-
+const chefs = require('./chefs_list.json')
 app.get('/', (req, res) =>{
     res.send('Bangla spice is running')
+});
+app.get('/chefs', (req, res) =>{
+    res.send(chefs)
 });
 
 app.listen(port, () => {
