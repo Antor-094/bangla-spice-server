fef8ab9d2fb0207ec7fc5e6f9bbf61c8f1fffc50
Antor-094 <mdantor556677889@gmail.com>
2023-05-03 19:48:22 +0600

    get the single chefs recipes api created

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const port = process.env.PORT || 5000;
+const cors = require('cors')
 const app = express()
 const chefs = require('./chefs_list.json')
+
+app.use(cors())
+
 app.get('/', (req, res) =>{
     res.send('Bangla spice is running')
 });
 app.get('/chefs', (req, res) =>{
     res.send(chefs)
+});
+app.get('/chefs/:id', (req, res) =>{
+    const id = req.params.id;
+    const selectedChef = chefs.find(n => n.id === id);
+    res.send(selectedChef)
 });
 
 app.listen(port, () => {
